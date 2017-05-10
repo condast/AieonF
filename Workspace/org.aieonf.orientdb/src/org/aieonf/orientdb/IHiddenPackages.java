493554e45ee4d81195607f229bfca9035e5339e8
Kees Pieters <info@condast.com>
2017-05-10 11:22:03 +0200

    OrientDb updated to version 2.16

---
+++ Workspace/org.aieonf.orientdb/src/org/aieonf/orientdb/IHiddenPackages.java
@@ -0,0 +1,14 @@
+package org.aieonf.orientdb;
+
+/**
+ * Packages that fail the dependency analysis
+ * @author Kees
+ *
+ */
+import org.aieonf.commons.filter.*;
+import org.aieonf.concept.locator.*;
+
+@SuppressWarnings("unused")
+public interface IHiddenPackages {
+
+}
