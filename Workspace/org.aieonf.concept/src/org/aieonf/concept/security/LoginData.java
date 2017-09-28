fa5499f035a021c1e3a8b47077b73968c05b71d4
Kees <info@condast.com>
2017-09-28 22:18:27 +0200

    preparation for opening cache with login

--- Workspace/org.aieonf.concept/src/org/aieonf/concept/security/LoginData.java
+++ Workspace/org.aieonf.concept/src/org/aieonf/concept/security/LoginData.java
@@ -3,18 +3,20 @@ package org.aieonf.concept.security;
 public class LoginData
 {
 	private String loginName;
+	private String password;
 
-	
 	public LoginData()
 	{
 		super();
 		this.loginName = null;
+		this.password = null;
 	}
 
-	public LoginData( String loginName )
+	public LoginData( String loginName, String password )
 	{
 		super();
 		this.loginName = loginName;
+		this.password = password;
 	}
 
 	public boolean isLoggedIn()
@@ -25,5 +27,9 @@ public class LoginData
 	public String getLoginName()
 	{
 		return this.loginName;
+	}
+
+	public synchronized String getPassword() {
+		return password;
 	}
 }
